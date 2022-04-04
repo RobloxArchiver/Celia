14b4c789736518ee97f0b429ec0db5084244640f
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-04-04 08:02:00 -0400

    Added `StreamerMode`. + New Template

--- template/MainTemplate.lua
+++ template/MainTemplate.lua
@@ -1,28 +1,31 @@
+--[[
+
+-->
+--> _G, getfenv, and local table changes;
+-->     getfenv().streamerMode (set to false normally unless toggled by user.)
+-->
+
+]]
+
+--> Variables
 local Player = game:GetService("Players").LocalPlayer;
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name;
 local WSpace = game:GetService("Workspace");
 
---[[
- 
- GUI SHIT AFTER THIS
- 
- ]]
-
---#region // DO NOT CHANGE
+--> Loader
 local Celia = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/cc77cf7e-a9ae-407b-b0f9-066000ddd648/main/5bc53455-1a45-4a19-bd89-890c2ccaa3f6.lua"))();
 local CeliaWindow = Celia.new(true, "Celia 💚");
 CeliaWindow.ChangeToggleKey(Enum.KeyCode.BackSlash);
---#endregion
 
+--> Categories
 local WelcomeCategory = CeliaWindow:Category("Welcome")
 
---#region // Welcome
+--> Welcome
 local Welcome = WelcomeCategory:Sector("Welcome")
 Welcome:Cheat("Label", "Celia Loaded!")
 Welcome:Cheat("Label", "Made by Uptight")
 Welcome:Cheat("Label", "Keybind: BackSlash")
 local Information = WelcomeCategory:Sector("Information")
-Information:Cheat("Label", Player.Name)
-Information:Cheat("Label", Player.UserId)
+--[[Streamer Mode, displays ***** instead of the user.]] if getfenv().streamerMode==true then Information:Cheat("Label","******")else Information:Cheat("Label",Player.Name)end
+--[[Streamer Mode, its stupid but must keep tradition.]] if getfenv().streamerMode==true then Information:Cheat("Label","******")else Information:Cheat("Label",Player.DisplayName)end
 Information:Cheat("Label", GameName)
---#endregion
